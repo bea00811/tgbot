ee8567cfa5b9506c332425b6fa7dbca04e908ad7
bea00811 <irinakozlovawork@gmail.com>
2025-04-05 11:23:47 +0300

    changes for test

---
+++ tg-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TelegramMessageCounter from './TelegramMessageCounter'
+//  import TelegramBot from './TelegramBot'
+function App() {
+  return (
+    <>
+    {/* { <TelegramBot/>} */}
+     <TelegramMessageCounter/>
+    </>
+  )
+}
+
+export default App
---
+++ tg-app/src/TelegramMessageCounter.tsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+
+// Определение типов для ответа API
+interface TelegramUpdate {
+  message?: {
+    chat: {
+      type: 'private' | 'group'
+    }
+  };
+}
+
+const TelegramMessageCounter: React.FC = () => {
+  const [messageCount, setMessageCount] = useState<number | null>(null);
+  const [status, setStatus] = useState<string>('');
+
+  const token: string = '7860280378:AAE12iDkjYJTy2gpG8RpPoiJDV9wRN6CdJc'; // Замените на ваш токен
+
+  // Асинхронная функция для получения обновлений
+  const getUpdates = async (token:string): Promise<TelegramUpdate[]> => {
+    try {
+      const response = await fetch(`https://api.telegram.org/bot${token}/getUpdates`);
+      const data = await response.json();
+      console.log(data.result)
+      console.log('data')
+      return data.result;
+    } catch (error) {
+      console.error('Error fetching updates:', error);
+      return [];
+    }
+  };
+
+  // Функция для подсчета сообщений
+  const countMessages = (updates: TelegramUpdate[]): number => {
+    let count = 0;
+    updates.forEach(update => {
+      if (update.message && (update.message.chat.type === 'private' || update.message.chat.type === 'group')) {
+        count++;
+      }
+    });
+    return count;
+  };
+
+  // Функция, вызываемая при изменении видимости элемента
+  const handleVisibilityChange = async () => {
+    setStatus('Подсчет сообщений');
+    const updates = await getUpdates(token);
+    const count = countMessages(updates);
+    setMessageCount(count);
+    setStatus(`Число сообщений: ${count}`);
+   };
+
+   
+
+  // Использование IntersectionObserver для отслеживания видимости элемента 'chat'
+  useEffect(() => {
+    const chatElement = document.getElementById('chat');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          handleVisibilityChange();
+        }
+      });
+    });
+
+    if (chatElement) {
+      observer.observe(chatElement);
+    }
+
+    return () => {
+      if (chatElement) {
+        observer.unobserve(chatElement);
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+       <div id="chat" style={{ height: '200px', overflow: 'auto' }}>
+        {/* Чат-контент */}
+      </div>
+      <div>
+          {status} 
+      </div>
+    </div>
+  );
+};
+
+export default TelegramMessageCounter;
